c9f5c20172114d93d8b998217dced4264731032d
Alexandr Folomeshkin <fifax.adriano@gmail.com>
2022-02-13 01:08:42 +0600

    added module2 project

---
+++ Assets/Scripts/CharacterAnimationEvents.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimationEvents : MonoBehaviour
+{
+    Character character;
+
+    void Start()
+    {
+        character = GetComponentInParent<Character>();
+    }
+
+    void ShootEnd()
+    {
+        character.SetState(Character.State.Idle);
+    }
+
+    void AttackEnd()
+    {
+        character.SetState(Character.State.RunningFromEnemy);
+    }
+
+    void EnemyDeath()
+    {
+        character.enemy.SetState(Character.State.Death);
+    }
+
+    void ZombieAttackEnd()
+    {
+        character.SetState(Character.State.Idle);
+    }
+}
